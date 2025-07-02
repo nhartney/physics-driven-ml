8ac3df59b60eb5b749e8b7ec92a1e26d39ead023
nhartney <nh491@exeter.ac.uk>
2025-07-02 14:22:46 +0100

    make generating training and testing data the main method

--- physics_driven_ml/dataset_processing/generate_heat_eqn_example_data.py
+++ physics_driven_ml/dataset_processing/generate_heat_eqn_example_data.py
@@ -10,6 +10,8 @@ from numpy import random
 from tqdm.auto import tqdm, trange
 from sklearn.model_selection import train_test_split
 
+from physics_driven_ml.utils import get_logger
+
 def advance_one_timestep(u_in, mesh, V, bcs, time):
     k = Constant(1.)
     dt = Constant(0.1)
@@ -49,8 +51,9 @@ def solve_with_IC(mesh, ntimesteps, dt, IC):
 
 
 # Define the list of initial conditions to use to generate solutions from
-def generate_initial_conditions(n):
+def generate_initial_conditions(mesh, n):
     ICs_list = []
+    x,y = SpatialCoordinate(mesh)
     # Produce n random samples for initial conditions
     for r in range(n):
         x_pos = random.rand()
@@ -61,44 +64,45 @@ def generate_initial_conditions(n):
     return ICs_list
 
 
-# Set up test problem
-# Domain
-Lx = Ly = 1
-nx = ny = 5
-mesh = RectangleMesh(nx, ny, Lx, Ly, name="mesh")
-x,y = SpatialCoordinate(mesh)
-dt = 0.001
+if __name__ == "__main__":
+    logger = get_logger("Generating synthetic train and test data")
+    # Set up test problem
+    # Domain
+    Lx = Ly = 1
+    nx = ny = 5
+    mesh = RectangleMesh(nx, ny, Lx, Ly, name="mesh")
+    dt = 0.001
 
-# Produce u, f and t for 10 timesteps, for a range of initial conditions
-point_data_list = []
-global_data_list = []
-initial_conditions = generate_initial_conditions(5)
+    # Produce u, f and t for 10 timesteps, for a range of initial conditions
+    point_data_list = []
+    global_data_list = []
+    initial_conditions = generate_initial_conditions(mesh, 5)
 
-for IC in initial_conditions:
-    sln = solve_with_IC(mesh=mesh, ntimesteps=10, dt=dt, IC=IC)
-    # extract f and u at (x,y) points from (f,u,t) solutions
-    for s in sln:
-        f = s[0]
-        u = s[1]
-        t = s[2]
-        for i, j in mesh.coordinates.dat.data:
-            f_eval = f.at(i,j)
-            u_eval = u.at(i,j)
-            # concatenate list of (f,u,t,x,y) solutions
-            point_data_list.append((f_eval, u_eval, t, i, j))
-        # concatenate global data as a list of (f,u,t) functions
-        global_data_list.append((s[0], s[1], t))
+    for IC in initial_conditions:
+        sln = solve_with_IC(mesh=mesh, ntimesteps=10, dt=dt, IC=IC)
+        # extract f and u at (x,y) points from (f,u,t) solutions
+        for s in sln:
+            f = s[0]
+            u = s[1]
+            t = s[2]
+            for i, j in mesh.coordinates.dat.data:
+                f_eval = f.at(i,j)
+                u_eval = u.at(i,j)
+                # concatenate list of (f,u,t,x,y) solutions
+                point_data_list.append((f_eval, u_eval, t, i, j))
+            # concatenate global data as a list of (f,u,t) functions
+            global_data_list.append((s[0], s[1], t))
 
 
-# Split point data into train and test sets using scikitlearn
-train_pointwise, test_pointwise = train_test_split(point_data_list, test_size=0.2, random_state=42)
-print("Number of point-data training examples:", len(train_pointwise))
-print("Number of point-data testing examples:", len(test_pointwise))
+    # Split point data into train and test sets using scikitlearn
+    train_pointwise, test_pointwise = train_test_split(point_data_list, test_size=0.2, random_state=42)
+    print("Number of point-data training examples:", len(train_pointwise))
+    print("Number of point-data testing examples:", len(test_pointwise))
 
-# Save the data in train and test sets as numpy arrays
-dataset_dir = os.path.join(
-        "/Users/Jemma/Nell/code/physics-driven-ml/data/datasets",
-        "heat_problem_example_data")
-# Save pointdata as numpy arrays
-np.save(os.path.join(dataset_dir, 'numpy_point_train_data'), train_pointwise)
-np.save(os.path.join(dataset_dir, 'numpy_point_test_data'), test_pointwise)
+    # Save the data in train and test sets as numpy arrays
+    dataset_dir = os.path.join(
+            "/Users/Jemma/Nell/code/physics-driven-ml/data/datasets",
+            "heat_problem_example_data")
+    # Save pointdata as numpy arrays
+    np.save(os.path.join(dataset_dir, 'numpy_point_train_data'), train_pointwise)
+    np.save(os.path.join(dataset_dir, 'numpy_point_test_data'), test_pointwise)

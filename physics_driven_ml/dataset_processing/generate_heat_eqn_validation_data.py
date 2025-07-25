19dae4cfc50f2d6a9ab5673158adf5d196b373af
nhartney <nh491@exeter.ac.uk>
2025-07-25 17:18:05 +0100

    add global fields to validation data generation

--- physics_driven_ml/dataset_processing/generate_heat_eqn_validation_data.py
+++ physics_driven_ml/dataset_processing/generate_heat_eqn_validation_data.py
@@ -4,7 +4,7 @@ initial conditions. The equation is solved in time with the forward Euler method
 """
 import os
 import numpy as np
-from firedrake import RectangleMesh, SpatialCoordinate
+from firedrake import RectangleMesh, SpatialCoordinate, CheckpointFile
 from generate_heat_eqn_example_data import generate_initial_conditions, solve_with_IC
 
 # Set up test problem
@@ -15,12 +15,20 @@ mesh = RectangleMesh(nx, ny, Lx, Ly, name="mesh")
 # x,y = SpatialCoordinate(mesh)
 dt = 0.0015
 
+nICs = 3
+ntimesteps = 5
+
+n_global_validate = nICs*ntimesteps
+
 # Produce u, f and t for 10 timesteps, for a range of initial conditions
 point_data_list = []
-initial_conditions = generate_initial_conditions(mesh, 1)
+global_data_list = []
+label = 0 
+
+initial_conditions = generate_initial_conditions(mesh, nICs)
 
 for IC in initial_conditions:
-    sln = solve_with_IC(mesh=mesh, ntimesteps=5, dt=dt, IC=IC)
+    sln = solve_with_IC(mesh=mesh, ntimesteps=ntimesteps, dt=dt, IC=IC)
     # extract f and u at (x,y) points from (f,u,t) solutions
     for s in sln:
         f = s[0]
@@ -30,12 +38,22 @@ for IC in initial_conditions:
             f_eval = f.at(i,j)
             u_eval = u.at(i,j)
             # concatenate list of (f,u,t,x,y) solutions
-            point_data_list.append((f_eval, u_eval, t, i, j))
+            point_data_list.append((f_eval, u_eval, t, i, j, label))
+        global_data_list.append((f, label))
 
-# Save the data as numpy arrays
+# Save the point data as numpy array
 dataset_dir = os.path.join(
         "/Users/Jemma/Nell/code/physics-driven-ml/data/datasets",
-        "heat_problem_validation_data")
-# Save pointdata as numpy arrays
+        "heat_problem_global_validation_data")
 print("Number of point-data validation examples:", len(point_data_list))
 np.save(os.path.join(dataset_dir, 'numpy_point_validate_data'), point_data_list)
+
+# Save the global data to checkpoint file
+global_validate_dir = os.path.join(dataset_dir, "global_validate_data.h5")
+global_val = global_data_list[:n_global_validate]
+print("Number of global data validation examples:", len(global_data_list))
+with CheckpointFile(global_validate_dir, "w") as afile:
+        afile.h5pyfile["n"] = len(global_val)
+        afile.save_mesh(mesh)
+        for i, (f, label) in enumerate(global_val):
+            afile.save_function(f, idx=i, name="target_f")

f506e4fd09a1964f70506a112a996d79e0c63430
nhartney <nh491@exeter.ac.uk>
2025-07-28 15:16:14 +0100

    fix lint

--- physics_driven_ml/models/point_nn.py
+++ physics_driven_ml/models/point_nn.py
@@ -12,22 +12,21 @@ class PointNN(Module):
         super().__init__()
 
         self.nn_encoder = Sequential(Linear(3, 32),
-                                    ReLU(True),
-                                    Linear(32, 64),
-                                    ReLU(True),
-                                    Linear(64, 128),
-                                    ReLU(True))
+                                     ReLU(True),
+                                     Linear(32, 64),
+                                     ReLU(True),
+                                     Linear(64, 128),
+                                     ReLU(True))
 
         self.nn_decoder = Sequential(Linear(128, 64),
-                                    ReLU(True),
-                                    Linear(64, 32),
-                                    ReLU(True),
-                                    Linear(32, 1),
-                                    Tanh())
-        
+                                     ReLU(True),
+                                     Linear(64, 32),
+                                     ReLU(True),
+                                     Linear(32, 1),
+                                     Tanh())
 
     def forward(self, input_tensor):
-    
+
         # CNN encoder-decoder
         z = self.nn_encoder(input_tensor)
         y = self.nn_decoder(z)

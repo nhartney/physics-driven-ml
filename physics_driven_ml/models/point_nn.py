d07a8ccabfca1ef5f0b88452ef1a5eb2078ed1ee
nhartney <nh491@exeter.ac.uk>
2025-07-28 15:13:46 +0100

    a NN for training pointwise, with 3 inpnuts and one output

---
+++ physics_driven_ml/models/point_nn.py
@@ -0,0 +1,35 @@
+from torch.nn import Module, Sequential, Linear, ReLU, Tanh
+
+
+class PointNN(Module):
+    """
+    Build a simple toy nn-based model to deal with point data. The network takes 3
+    inputs and returns one output. It has 4 hidden layers, each followed by a ReLU
+    activation function.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.nn_encoder = Sequential(Linear(3, 32),
+                                    ReLU(True),
+                                    Linear(32, 64),
+                                    ReLU(True),
+                                    Linear(64, 128),
+                                    ReLU(True))
+
+        self.nn_decoder = Sequential(Linear(128, 64),
+                                    ReLU(True),
+                                    Linear(64, 32),
+                                    ReLU(True),
+                                    Linear(32, 1),
+                                    Tanh())
+        
+
+    def forward(self, input_tensor):
+    
+        # CNN encoder-decoder
+        z = self.nn_encoder(input_tensor)
+        y = self.nn_decoder(z)
+
+        return y

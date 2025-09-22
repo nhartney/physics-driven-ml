d507cac2e8ece43870117cc9100debc11c58c558
nhartney <nh491@exeter.ac.uk>
2025-09-22 10:03:22 +0100

    simple point NN for debugging

--- physics_driven_ml/models/point_cnn.py
+++ physics_driven_ml/models/point_cnn.py
@@ -28,3 +28,24 @@ class PointNN(Module):
         y = self.nn_decoder(z)
 
         return y
+    
+
+class SimplePointNN(Module):
+    """Build an even simpler toy nn-based model to deal with point data for debugging purposes"""
+
+    def __init__(self):
+        super().__init__()
+
+        self.nn_encoder = Sequential(Linear(4, 4),
+                                    ReLU(True))
+
+        self.nn_decoder = Sequential(Linear(4, 1))
+        
+
+    def forward(self, input_tensor):
+    
+        # CNN encoder-decoder
+        z = self.nn_encoder(input_tensor)
+        y = self.nn_decoder(z)
+
+        return y
